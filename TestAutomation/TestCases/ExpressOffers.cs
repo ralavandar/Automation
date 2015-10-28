a43b3e239972b2cc65d3e35d6c1bf82704110fd9
Maryanne Sweat <maryanne.sweat@lendingtree.com>
2015-10-28 15:25:03 -0400

    ExpressOffer Automation InitialBuild
    ExpressOffer Automation InitialBuild

---
+++ TestAutomation/TestCases/ExpressOffers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TestAutomation.Lending;
+
+namespace TestAutomation.LendingTree.ExpressOffers
+{
+     [TestFixture]
+    class ExpressOffers: SeleniumTestBase
+    {
+
+       [Test]
+       public void PurchaseQFCreate()
+        {
+            BackDoorFormSubmit newQF =new BackDoorFormSubmit();
+            newQF.FormSubmit();
+
+        }
+    }
+}
