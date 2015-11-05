fedb1df995b1ba1ca17c353fb229b0c58b794382
ob1schupp <mschupp@lendingtree.com>
2015-11-05 15:26:36 -0500

    Adding the secret sauce
    Quick test on Sauce Labs

--- TestAutomation/MyDevTests/TestCloud.cs
+++ TestAutomation/MyDevTests/TestCloud.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 
 using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
 
@@ -14,19 +15,34 @@ namespace TestAutomation.LendingTree.zSandbox
     [TestFixture]
     public class TestCloud : SeleniumTestBase
     {
-        private IWebDriver driver;
+        private IWebDriver _Driver;
 
         [SetUp]
         public void SetupTest()
         {
             Common.InitializeTestResults();
-            driver = StartBrowser("FIREFOX");
+
+            // construct the url to sauce labs
+            Uri commandExecutorUri = new Uri("http://ondemand.saucelabs.com/wd/hub");
+            // set up the desired capabilities
+            DesiredCapabilities desiredCapabilites = new DesiredCapabilities("chrome", "35", Platform.CurrentPlatform); // set the desired browser
+            desiredCapabilites.SetCapability("platform", "linux"); // operating system to use
+            desiredCapabilites.SetCapability("username", "tdakhla"); // supply sauce labs username
+            desiredCapabilites.SetCapability("accessKey", "41f99617-74dd-4c9d-a4af-e126becb6182");  // supply sauce labs account key
+            desiredCapabilites.SetCapability("name", TestContext.CurrentContext.Test.Name); // give the test a name
+
+            // start a new remote web driver session on sauce labs
+            _Driver = new RemoteWebDriver(commandExecutorUri, desiredCapabilites);
+            _Driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+
+            // navigate to the page under test
+            _Driver.Navigate().GoToUrl("https://saucelabs.com/test/guinea-pig");
         }
 
         [TearDown]
         public void TeardownTest()
         {
-            driver.Quit();
+            _Driver.Quit();
             Common.ReportFinalResults();
         }
 
@@ -35,17 +51,17 @@ namespace TestAutomation.LendingTree.zSandbox
         public void LinkTest_01_Disclosures()
         {
             // Navigate to the form
-            driver.Navigate().GoToUrl("https://offers.staging.lendingtree.com/tlm.aspx?tid=m2&esourceid=14349");
+            _Driver.Navigate().GoToUrl("https://offers.staging.lendingtree.com/tlm.aspx?tid=m2&esourceid=14349");
             System.Threading.Thread.Sleep(2000);
 
             // Click Disclosures link
-            driver.FindElement(By.LinkText("Disclosures")).Click();
+            _Driver.FindElement(By.LinkText("Disclosures")).Click();
             System.Threading.Thread.Sleep(5000);
 
             // Verify 
-            driver.SwitchTo().Window(driver.WindowHandles.Last());
-            Validation.StringCompare("https://www.lendingtree.com/legal/advertising-disclosures", driver.Url);
-           
+            _Driver.SwitchTo().Window(_Driver.WindowHandles.Last());
+            Validation.StringCompare("https://www.lendingtree.com/legal/advertising-disclosures", _Driver.Url);
+
         }
 
     }
